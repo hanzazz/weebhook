540233c09ab3c3467f579555eb277edfaa1d02d9
hanzazz <we__j3ack@hotmail.co.th>
2020-09-03 18:30:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -88,18 +88,6 @@ if ( sizeof($request_array['events']) > 0 ) {
             }
             
         }
-        else{
-          $data = [
-              'replyToken' => $token,
-              // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-              'messages' => [['type' => 'text', 'text' => "Keyword no Correct!! Check Pls" ]]
-          ];
-          $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-          $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-          echo "Result: ".$send_result."\r\n";
-        }
         
     }
 }
