f3d2bfe2da53a4edbeadb28b1069dc6c640df9f1
hanzazz <we__j3ack@hotmail.co.th>
2020-09-03 18:20:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,11 @@ $dbname = "LINEIN";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
+/*
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
+*/
 
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'XVkmOR4aT771B9CnIdxvdGmlOtXQSijnvLZ+T7GC5Hd8cVC8nKslvKPBTUs2M6vI5WhhF92i6S1NvR/ZY7IARrfIWCCZwo+ZYk6bzTnL9+ilJOWBlQyPXUvlZvgR5eE3a2KZ+C+hhDLn7bbiDVUJQgdB04t89/1O/w1cDnyilFU='; 
