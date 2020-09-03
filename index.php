5b56503a45b995df4383aad9911db0f03c6d8470
hanzazz <we__j3ack@hotmail.co.th>
2020-09-03 18:46:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -38,50 +38,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         echo "Result: ".$send_result."\r\n";
       
-        if($text_S == "EU-1" || $text_S == "AU-1" || $text_S == "GU-1" || $text_S == "NU-1" || $text_S == "UCAD-1" || $text_S == "UCHF-1" || $text_S == "UJPY-1" ||
-           $text_S == "eu-1" || $text_S == "au-1" || $text_S == "gu-1" || $text_S == "nu-1" || $text_S == "ucad-1" || $text_S == "uchf-1" || $text_S == "ujpy-1" ||
-           $text_S == "EU-2" || $text_S == "AU-2" || $text_S == "GU-2" || $text_S == "NU-2" || $text_S == "UCAD-2" || $text_S == "UCHF-2" || $text_S == "UJPY-2" ||
-           $text_S == "eu-2" || $text_S == "au-2" || $text_S == "gu-2" || $text_S == "nu-2" || $text_S == "ucad-2" || $text_S == "uchf-2" || $text_S == "ujpy-2" ){
         
-            $x = 0;
-            /**************************************************************************** */
-            //if (err) throw err;
-            //Select all customers and return the result object:
-            $sql = "SELECT * FROM log";
-            $result = $conn->query($sql);
-            
-            while($row = mysqli_fetch_assoc($result)) {
-                
-                $UDI = $row["UserID"];
-                $GROUPID = $row["GroupID"];
-                if($userID == $UDI){
-                    $sql = "UPDATE log SET  Text='$text' WHERE UserID='$userID' AND GroupID='$groupID'";
-                    if ($conn->query($sql) === TRUE) {
-                        echo "UserID: ".$userID."  updated successfully";
-                        handleEvent($reply_token);
-                    } else {
-                        echo "UserID: ".$userID."  updated Error" . $conn->error;
-                    }
-                    
-                    $x =0;
-                    break;
-                }
-                else $x =1;
-            }
-            if($x==1){
-                if($userID != $UDI){
-                    $sql = "INSERT INTO log (UserID, Text, Timestamp, GroupID) VALUES ('$userID','$text', '$timestamp','$groupID')" ;
-                    if ($conn->query($sql) === TRUE) {
-                        echo "New UserID: ".$userID."New record created successfully";
-                        handleEvent($reply_token);
-                    } else {
-                        echo "New UserID: ".$userID."New record created Error". $conn->error;
-                    }
-                    
-                }
-            }
-            
-        }
         
     }
 }
