ea4a414135f2f959111fec732d192420d371649e
hanzazz <we__j3ack@hotmail.co.th>
2020-09-07 11:40:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if ( sizeof($request_array['events']) > 0 ) {
                 $data = [
                     'replyToken' => $reply_token,
                     // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-                    'messages' => [['type' => 'text', 'text' => $UDI ]]
+                    'messages' => [['type' => 'text', 'text' => "TEST ".$UDI ]]
                 ];
                 $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
