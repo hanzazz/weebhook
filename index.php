6a0b72261128f6ae9877fc9fd165e7d1235a7ca6
hanzazz <we__j3ack@hotmail.co.th>
2020-09-07 13:36:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,32 +13,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$sql = "INSERT INTO log (UserID, Account, Text,Timestamp,GroupID) VALUES ('1','2', '3','4','5')" ;
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-    $data = [
-        'replyToken' => $reply_token,
-        // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-        'messages' => [['type' => 'text', 'text' => "New record created successfully" ]]
-    ];
-    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-    $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-    echo "Result: ".$send_result."\r\n";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-    $data = [
-        'replyToken' => $reply_token,
-        // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-        'messages' => [['type' => 'text', 'text' => "Error: " . $sql . "<br>" . $conn->error ]]
-    ];
-    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-    $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-    echo "Result: ".$send_result."\r\n";
-}
 
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'XVkmOR4aT771B9CnIdxvdGmlOtXQSijnvLZ+T7GC5Hd8cVC8nKslvKPBTUs2M6vI5WhhF92i6S1NvR/ZY7IARrfIWCCZwo+ZYk6bzTnL9+ilJOWBlQyPXUvlZvgR5eE3a2KZ+C+hhDLn7bbiDVUJQgdB04t89/1O/w1cDnyilFU='; 
@@ -73,7 +47,33 @@ if ( sizeof($request_array['events']) > 0 ) {
         
             $x = 0;
             //*************************************************************************** */
-            
+            $sql = "INSERT INTO log (UserID, Account, Text,Timestamp,GroupID) VALUES ('1','2', '3','4','5')" ;
+            if ($conn->query($sql) === TRUE) {
+              echo "New record created successfully";
+                $data = [
+                    'replyToken' => $reply_token,
+                    // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
+                    'messages' => [['type' => 'text', 'text' => "New record created successfully" ]]
+                ];
+                $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+                $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+                echo "Result: ".$send_result."\r\n";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+                $data = [
+                    'replyToken' => $reply_token,
+                    // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
+                    'messages' => [['type' => 'text', 'text' => "Error: " . $sql . "<br>" . $conn->error ]]
+                ];
+                $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+                $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+                echo "Result: ".$send_result."\r\n";
+            }
+
             /**************************************************************************** */
             //if (err) throw err;
             //Select all customers and return the result object:
