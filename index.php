82377ca0448deed7363a9ed6e7486ede7dc96097
hanzazz <we__j3ack@hotmail.co.th>
2020-09-07 13:59:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -60,39 +60,42 @@ if ( sizeof($request_array['events']) > 0 ) {
                 //console.log(`A JavaScript type is: ${result[_ID]["UserID"]}`)
                 $UDI = $result[$i]["UserID"];
                 $GROUPID = $result[$i]["GroupID"];
+                //*************************************************************************** */
+                $sql = "INSERT INTO log (UserID, Account, Text,Timestamp,GroupID) VALUES ('1','2', $userID,'4','5')" ;
+                //$sql = "UPDATE log SET  Text='ttt' WHERE UserID='$userID' AND GroupID='$groupID'";
+                if ($conn->query($sql) === TRUE) {
+                    echo "New record created successfully";
+                    $data = [
+                        'replyToken' => $reply_token,
+                        // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
+                        'messages' => [['type' => 'text', 'text' => "wait pls!!" ]]
+                    ];
+                    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
+                    $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+                    echo "Result: ".$send_result."\r\n";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                    $data = [
+                        'replyToken' => $reply_token,
+                        // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
+                        'messages' => [['type' => 'text', 'text' => "Error: " . $sql . "<br>" . $conn->error ]]
+                    ];
+                    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+                    $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+                    echo "Result: ".$send_result."\r\n";
+                }
+
+                /**************************************************************************** */
                 if($userID == $UDI){
                     
                     
                     //*************************************************************************** */
                     //$sql = "INSERT INTO log (UserID, Account, Text,Timestamp,GroupID) VALUES ('1','2', '3','4','5')" ;
-                    $sql = "UPDATE log SET  Text='ttt' WHERE UserID='$userID' AND GroupID='$groupID'";
-                    if ($conn->query($sql) === TRUE) {
-                        echo "New record created successfully";
-                        $data = [
-                            'replyToken' => $reply_token,
-                            // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-                            'messages' => [['type' => 'text', 'text' => "wait pls!!" ]]
-                        ];
-                        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-                        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-                        echo "Result: ".$send_result."\r\n";
-                    } else {
-                        echo "Error: " . $sql . "<br>" . $conn->error;
-                        $data = [
-                            'replyToken' => $reply_token,
-                            // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-                            'messages' => [['type' => 'text', 'text' => "Error: " . $sql . "<br>" . $conn->error ]]
-                        ];
-                        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-                        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-                        echo "Result: ".$send_result."\r\n";
-                    }
-
+                    
                     /**************************************************************************** */
                     $x =0;
                     break;
